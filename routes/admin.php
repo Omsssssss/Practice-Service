90fd8c66529b877bb8b6685fa509d31662076c4a
Omsssssss <160719104+Omsssssss@users.noreply.github.com>
2024-06-27 11:11:28 +0800

    a

--- routes/admin.php
+++ routes/admin.php
@@ -16,16 +16,15 @@ use App\Model\Admin;
 Route::middleware('guest:admin')->prefix('admin')->name('admin.')->group(function () {
 
 	Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
-
-	Route::post('register', [RegisteredUserController::class, 'store']);
+ 
 
 	Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
 
 	Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-	Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('admin.password.request');
+	Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
 
-	Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('admin.password.email');
+	Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
 
 	Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
 
